fca3c6f7b8a254c2952ecb57beb939a6f7f32348
Shoaib Ul Haq <shoaibulhaqq@outlook.com>
2024-08-24 12:46:05 +0500

    Single User

--- customers/urls.py
+++ customers/urls.py
@@ -5,4 +5,5 @@ from customers import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/customers/", views.customers, name="customers"),
+    path("api/customers/<int:id>", views.customer, name="customer"),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,5 @@
 from customers.models import Customer
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from customers.serializers import CustomerSerializer
 
 
@@ -9,4 +9,16 @@ def customers(request):
     serializer = CustomerSerializer(data, many=True)
     return JsonResponse(
         {"customers": serializer.data}
+    )  # This is going to be the json compatible version that we are going to pass as response
+
+
+# Function to get a single customer
+def customer(request, id):
+    try:
+        data = Customer.objects.get(pk=id)  # searching with id
+    except Customer.DoesNotExist:
+        raise Http404("Customer does not exist")
+    serializer = CustomerSerializer(data)
+    return JsonResponse(
+        {"customer": serializer.data}
     )  # This is going to be the json compatible version that we are going to pass as response
