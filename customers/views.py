3697b82faca132a0056ecef9d5dda32fa4e17246
Shoaib Ul Haq <shoaibulhaqq@outlook.com>
2024-09-02 15:44:48 +0500

    Added a register view

--- customers/views.py
+++ customers/views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 @api_view(["GET", "POST"])
@@ -67,5 +68,8 @@ def customer(request, id):
 def register(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        tokens = {"refresh": str(refresh), "access": str(refresh.access_token)}
+        return Response(tokens, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
